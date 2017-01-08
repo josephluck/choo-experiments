f2bdae39a4bd1cd1500c830f4ce2b641f19af3a0
Joseph Luck <joseph.luck@sky.com>
2017-01-08 18:04:23 +0000

    Wrap reducers and automatically generate component behaviour for a slicker api

--- components/Counter.js
+++ components/Counter.js
@@ -6,19 +6,7 @@ const counter = () => {
   return {
     // model.state is the default state for an instance of this
     // component, i.e. when a new counter is created, it will have
-    // this default state, if no initialState is passed in during
-    // its creation
-    //
-    // possibly remove instances so it's easier to
-    // consume / update state for these components?
-    // downside is that it could never be removed from
-    // global state using onunload since choo uses xtend
-    // under the hood which we cannot bypass
-    // or...
-    // might wanna wrap the reducer functions in choo-component
-    // so that they receive and return just their own instance
-    // state, that way 'id' and 'instances' are not exposed
-    // here and the end user does not need to be concerned about them
+    // this default state, if no initialState is passed
     model: {
       namespace: 'counter',
       state: {
@@ -26,48 +14,15 @@ const counter = () => {
         title: 'Default title'
       },
       reducers: {
-        increment (state, { id }) {
+        increment (state) {
           return {
-            instances: {
-              ...state.instances,
-              [id]: {
-                ...state.instances[id],
-                count: state.instances[id].count + 1
-              }
-            }
+            count: state.count + 1
           }
         },
-        updateTitle (state, { id, title }) {
+        updateTitle (state, { title }) {
           return {
-            instances: {
-              ...state.instances,
-              [id]: {
-                ...state.instances[id],
-                title
-              }
-            }
+            title
           }
-        }
-      }
-    },
-
-    // Behaviour to link view methods to reducers
-    // See above for comments on how to simplify reducers so that
-    // they do not need to know about id and instances
-    //
-    // We could also skip this behaviour step entirely by assuming
-    // that the name of the function is the same as the name of
-    // the reducer since behaviour only links view methods to
-    // reducers and reducers can only receive one payload argument
-    // we'd effectively be creating this object during the
-    // instantiation of the component
-    behaviour (send, id) {
-      return {
-        increment () {
-          send('counter:increment', { id })
-        },
-        updateTitle (title) {
-          send('counter:updateTitle', { id, title })
         }
       }
     },
@@ -92,7 +47,7 @@ const counter = () => {
           <input
             type="text"
             value=${title}
-            oninput=${(e) => updateTitle(e.target.value)}
+            oninput=${(e) => updateTitle({title: e.target.value})}
           />
           <br />
           ${title}
--- components/chooComponent.js
+++ components/chooComponent.js
@@ -1,51 +1,83 @@
 const html = require('choo/html')
 const assert = require('assert')
 
-const removeInstanceWithId = (instances, id) => {
-  const filteredInstances = Object.keys(instances)
-    .filter(key => key !== id)
-    .reduce((curr, prev) => Object.assign({}, instances[curr], prev), {})
-
-  return {
-    instances: filteredInstances
-  }
-}
-
-const updateInstanceWithId = (instances, id, value) => {
-  return {
-    instances: {
-      ...instances,
-      [id]: value
-    }
-  }
-}
-
+// Create onload and onunload reducers to initiate and
+// garbage collect an instance of a component when
+// onload and onunload lifecycle methods are called
+// setup sets the default state in the component
+// and teardown removes an instance from the state
 const getDefaultReducers = () => {
   return {
     setup (state, payload) {
-      return updateInstanceWithId(state.instances, payload.id, payload.initialState)
+      return {
+        instances: {
+          ...state.instances,
+          [payload.id]: payload.initialState
+        }
+      }
     },
     teardown (state, payload) {
-      return removeInstanceWithId(state.instances, payload.id)
+      return {
+        instances: Object.keys(state.instances)
+          .filter(key => key !== payload.id)
+          .reduce((curr, prev) => Object.assign({}, state.instances[curr], prev), {})
+      }
     }
   }
+}
+
+// Wrap reducers so that functions in the component
+// receive instance state rather than global state
+// note that for this to work, reducers need to be
+// send object payloads
+const generateReducers = (reducers) => {
+  return Object.keys(reducers).reduce((curr, key) => {
+    const enhancedPayload = (state, payload) => {
+      return {
+        instances: {
+          ...state.instances,
+          [payload.id]: {
+            ...state.instances[payload.id],
+            ...reducers[key](state.instances[payload.id], payload)
+          }
+        }
+      }
+    }
+    return {
+      ...curr,
+      [key]: enhancedPayload
+    }
+  }, {})
+}
+
+// Create glue between the components methods and
+// the components reducers.
+// note that for this to work, reducers need to be
+// send object payloads
+const generateBehaviour = (reducers, send, instanceId, namespace, state) => {
+  return Object.keys(reducers).reduce((curr, key) => {
+    return {
+      ...curr,
+      [key]: (payload) => {
+        assert.equal(typeof payload, 'object', `reducer ${key}: payload must be an object`)
+        send(`${namespace}:${key}`, {id: instanceId, ...payload})
+      }
+    }
+  }, {})
 }
 
 module.exports = (component) => {
   assert.equal(typeof component, 'object', 'Component must be an object')
   assert.equal(typeof component.view, 'function', 'Components view must be a function')
   assert.equal(typeof component.model, 'object', 'Components model must be an object')
-  assert.equal(typeof component.behaviour, 'function', 'Components behaviour must be a function')
   assert.equal(typeof component.model.namespace, 'string', 'Components model must have a namespace')
   assert.equal(typeof component.model.reducers, 'object', 'Components model have a reducers object')
 
-  const defaultReducers = getDefaultReducers()
-  const componentReducers = component.model.reducers ? component.model.reducers : {}
   const reducers = {
-    ...componentReducers,
-    ...defaultReducers
+    ...generateReducers(component.model.reducers ? component.model.reducers : {}),
+    ...getDefaultReducers()
   }
-  const effects = component.model.effects ? component.model.effects : {}
+
   return {
     model () {
       return {
@@ -54,14 +86,14 @@ module.exports = (component) => {
           instances: {}
         },
         reducers: reducers,
-        effects: effects
+        effects: component.model.effects ? component.model.effects : {}
       }
     },
     component (globalState, prev, send) {
       return function (instanceId, params) {
         assert.equal(typeof instanceId, 'string', 'Component instance must have an instanceId')
+        const behaviour = generateBehaviour(reducers, send, instanceId, component.model.namespace, globalState)
 
-        const behaviour = component.behaviour(send, instanceId)
         const props = params ? params.props : {}
         const currentInstanceState = globalState[component.model.namespace].instances[instanceId]
           ? globalState[component.model.namespace].instances[instanceId]
